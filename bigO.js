b11aec6de925c91172db088149fab9494973134e
jegbogu <jegbogu@gmail.com>
2024-03-12 04:50:00 +0100

    first commit

---
+++ bigO.js
@@ -0,0 +1,20 @@
+// const num  = [44,23,345,567]
+// let total = 0
+// for (let index = 0; index < num.length; index++) {
+//     total +=num[index]
+   
+    
+// }
+// console.log(total)
+
+//
+
+//object
+
+const Instructor = {
+    username: "Huston Lilian",
+    age: 34,
+    hobby:['football','music']
+}
+
+console.log(Object.keys(Instructor))
